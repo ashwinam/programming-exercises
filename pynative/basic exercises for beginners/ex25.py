ede2887a05494ed0227e99ebe2ad8067f28289ed
ashwinam <ashwinam31@gmail.com>
2026-03-31 06:37:52 +0530

    Add leap year checker implementation

---
+++ pynative/basic exercises for beginners/ex25.py
@@ -0,0 +1,8 @@
+"""Check Leap year"""
+
+def check_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 != 0:
+        return True
+    return False
+
+print(check_leap_year(2024))
